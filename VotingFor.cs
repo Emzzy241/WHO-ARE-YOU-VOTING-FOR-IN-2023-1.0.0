85ec41faf84a65ce8a651c7ef45c3fcc4eb43754
Emzzy241 <emzzyoluwole@gmail.com>
2023-02-22 14:17:31 +0100

    adding my C# Code file

---
+++ VotingFor.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+class VotingFor{
+    static void Main(){
+        // after the Main entry-point, let us write the code for our Voting App
+
+        Console.WriteLine("Welcome to the Voting App 101 b Dynasty");
+        
+        
+        
+        Console.WriteLine("With this Application being a small and scalable app, for now we would be focusing on users that want to vote only in 4 Parties which will be detailed below");
+
+        Console.WriteLine("Party 1: APC(All Progressive Congress)");
+        Console.WriteLine("Party 2: PDP(People's Democratic Party)");
+        Console.WriteLine("Party 3: LP(Labour Party)");
+        Console.WriteLine("Party 4: NNPP(Nigeria National People's Party)");
+
+        // Now describing the Political Aspirants for all of the listed Parties above
+
+        COnsole.WriteLine("The Flag Bearer of the APC is Bolaji Ahmed TInubu");
+        Console.WriteLine("The Flag Bearer of the PDP is Atiku Abubakar");
+        Console.WriteLine("The Presidential Aspirant of the LP is Peter Obi");
+        Console.WriteLine("The Flag Bearer of the NNPP is Kwankwaso");
+
+        // FInally the Logic to determine
+    }
+}
