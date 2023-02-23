4195da8e956572c5ddd57062466a35dc17ce2aed
Emzzy241 <emzzyoluwole@gmail.com>
2023-02-23 09:34:17 +0100

    added more code to Applications

--- VotingFor.cs
+++ VotingFor.cs
@@ -7,23 +7,25 @@ class VotingFor
     {
         // after the Main entry-point, let us write the code for our Voting App
 
-        Console.WriteLine("Welcome to the Voting App 101 b Dynasty");
+        Console.WriteLine("Welcome to the Voting App 101 by Dynasty");
 
 
+        // and also describing the Political Aspirants for all of the listed Parties above
 
-        Console.WriteLine("With this Application being a small and scalable app, for now we would be focusing on users that want to vote only in 4 Parties which will be detailed below");
+        Console.WriteLine("With this Application being a small but scalable app, for now we would be focusing on users that want to vote only in 4 Parties which will be detailed below");
 
-        Console.WriteLine("Party 1: APC(All Progressive Congress)");
-        Console.WriteLine("Party 2: PDP(People's Democratic Party)");
-        Console.WriteLine("Party 3: LP(Labour Party)");
-        Console.WriteLine("Party 4: NNPP(Nigeria National People's Party)");
+        Console.WriteLine("Party 1: APC(All Progressive Congress),The Flag Bearer of the APC is Bolaji Ahmed Tinubu");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Party 2: PDP(People's Democratic Party), The Flag Bearer of the PDP is Atiku Abubakar");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Party 3: LP(Labour Party), The Presidential Aspirant of the LP is Peter Obi");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Party 4: NNPP(Nigeria National People's Party), The Flag Bearer of the NNPP is Kwankwaso");
 
-        // Now describing the Political Aspirants for all of the listed Parties above
 
-        Console.WriteLine("The Flag Bearer of the APC is Bolaji Ahmed TInubu");
-        Console.WriteLine("The Flag Bearer of the PDP is Atiku Abubakar");
-        Console.WriteLine("The Presidential Aspirant of the LP is Peter Obi");
-        Console.WriteLine("The Flag Bearer of the NNPP is Kwankwaso");
 
         // FInally the Logic to determine Eligible Voting
         // using the C# comparison operator(==)
@@ -40,21 +42,36 @@ class VotingFor
 
 
         // string userAnsString = usersAns.GetTypeCode()
+        // In all my branches(if-statements) made reference to upper/lower case of the alphabets
+        usersAns = usersAns.ToUpper();
 
         if (usersAns == "Y")
         {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
             Console.WriteLine("Thanks for Continuing to use Dynasty's Small but scalable Voting App");
+            Console.WriteLine();
+            Console.WriteLine();
             Console.WriteLine("As we both Know, the Constitution of the Federal Republic of Nigeria says to exercise your Right to vote and be voted for, you must be 18 years of age and above");
-
+            Console.WriteLine();
+            Console.WriteLine();
             Console.WriteLine("We as an Organization will now be fperforming our Background Check to confirm you are 18 years of Age and Above");
-
+            Console.WriteLine();
+            Console.WriteLine();
             Console.WriteLine("To conntinue with this, Enter 'Y' for Yeah and 'N' No to exit the Application");
 
             string appContinue = Console.ReadLine();
+            appContinue = appContinue.ToUpper();
 
             if (appContinue == "Y")
             {
-                Console.WriteLine("Please answer the FOllowing Qustions");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Please answer the Following Questions");
                 Console.WriteLine("How Old Are you?");
 
                 string usersAge = Console.ReadLine();
@@ -116,18 +133,125 @@ class VotingFor
                 */
 
                 // Where I used both COnsole.WriteLine() and a break; statement and it worked perfectly
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
 
                 switch (intUsersAge)
                 {
-                     case >= 18:
+                    case >= 18:
                         Console.WriteLine("You are eligible to cast your Vote");
 
-                        
+
                         // Now After this, I now Know my user is of the right age to cast his/her vote
-                        
+
+                        // Section to determine THe Presidential Apirant my user would be voting for
+
+                        Console.WriteLine("Congratulations, My Dear User; YOu can cast your vote for any Political Aspirant of your choice");
+                        Console.WriteLine();
+                        Console.WriteLine("As a Little Bit of Summary Again, Here are the Political Aspirants you can VOte for");
+
+
+                        Console.WriteLine("Party 1: APC(All Progressive Congress),The Flag Bearer of the APC is Bolaji Ahmed TInubu");
+
+                        Console.WriteLine("Party 2: PDP(People's Democratic Party), The Flag Bearer of the PDP is Atiku Abubakar");
+                        Console.WriteLine("Party 3: LP(Labour Party), The Presidential Aspirant of the LP is Peter Obi");
+                        Console.WriteLine("Party 4: NNPP(Nigeria National People's Party), The Flag Bearer of the NNPP is Kwankwaso");
+
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine();
+
+                        Console.WriteLine("Now it is time to cast your vote. [Enter 'yes' to vote a Candidate, Enter No to Leave the Application]");
+
+                        string userVotes = Console.ReadLine();
+                        userVotes = userVotes.ToUpper();
+
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine();
+
+                        switch (userVotes)
+                        {
+                            case "YES":
+                                Console.WriteLine("Enter 'APC' to vote Bola Ahmed Tinubu as the next President of Nigeria");
+
+                                Console.WriteLine();
+                                Console.WriteLine();
+
+                                Console.WriteLine("Enter 'PDP' to vote Atiku Abubakar as the next President of Nigeria");
+
+                                Console.WriteLine();
+                                Console.WriteLine();
+
+                                Console.WriteLine("Enter 'LP' to vote Peter Obi as the next President of Nigeria");
+
+                                Console.WriteLine();
+                                Console.WriteLine();
+
+                                Console.WriteLine("Enter 'NNPP' to vote Kwankwaso as the next President of Nigeria");
+
+                                // Now I need to determine who my User has just voted for
+                                // Another switch case statement would do just that for me
+
+                                string whoUserVotes = Console.ReadLine();
+                                whoUserVotes = whoUserVotes.ToUpper();
+
+                                Console.WriteLine();
+                                Console.WriteLine();
+                                Console.WriteLine();
+
+                                switch (whoUserVotes)
+                                {
+                                    case "APC":
+                                        Console.WriteLine("Congratulations, You have successfully Voted (All Progressive Congress)APC whose Flag Bearer is Bola Ahmed TInubu");
+                                        Console.WriteLine();
+                                        Console.WriteLine();
+                                        Console.WriteLine("You are a Legend my Dear User and You've just Exercised your Right to Vote and Be Voted For which is in the Constitution");
+
+                                        break;
+                                    case "PDP":
+                                        Console.WriteLine("Congratulations, You have successfully Voted (People's Democratic Part)PDP whose Presidential Aspirant is Atiku Abubakar");
+                                        Console.WriteLine();
+                                        Console.WriteLine();
+                                        Console.WriteLine("You are a Legend my Dear User and You've just Exercised your Right to Vote and Be Voted For which is in the Constitution");
+
+
+                                        break;
+                                    case "LP":
+                                        Console.WriteLine("Congratulations, You have successfully Voted Labour Party(LP) whose Flag Bearer is Peter Obi");
+                                        Console.WriteLine();
+                                        Console.WriteLine();
+                                        Console.WriteLine("You are a Legend my Dear User and You've just Exercised your Right to Vote and Be Voted For which is in the Constitution");
+
+                                        break;
+                                    case "NNPP":
+                                        Console.WriteLine("Congratulations, You have successfully Voted Nigeria National People's Party(NNPP) whose Presidential Aspirant is Kwankwaso");
+                                        Console.WriteLine();
+                                        Console.WriteLine();
+                                        Console.WriteLine("You are a Legend my Dear User and You've just Exercised your Right to Vote and Be Voted For which is in the Constitution");
+
+                                        break;
+
+                                }
+
+
+                                break;
+                            case "NO":
+                                Console.WriteLine("We can Understand, you don't Want to vote any Longer, Goodbye My Dear User");
+                                break;
+
+                        }
+
+
                         break;
 
+
+
                     case 17:
+
                         Console.WriteLine("You only have one more year to go, Come back next Year to cast your Vote");
                         break;
 
@@ -142,7 +266,7 @@ class VotingFor
 
             }
             // exiting the Application for my user anytime they choose to
-            else if (usersAns == "N")
+            else if (appContinue == "N")
             {
                 Console.WriteLine("Goodbye Me Dear User :)");
             }
@@ -154,7 +278,5 @@ class VotingFor
         }
 
 
-
-
     }
-} 
+}
