3c813d3cc63c4f48f09c5e8d5b5734148f25a539
Emzzy241 <emzzyoluwole@gmail.com>
2023-02-23 04:47:04 +0100

    updating the C# code file with branches

--- VotingFor.cs
+++ VotingFor.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Collections.Generic;
 
-class VotingFor{
-    static void Main(){
+class VotingFor
+{
+    static void Main()
+    {
         // after the Main entry-point, let us write the code for our Voting App
 
         Console.WriteLine("Welcome to the Voting App 101 b Dynasty");
-        
-        
-        
+
+
+
         Console.WriteLine("With this Application being a small and scalable app, for now we would be focusing on users that want to vote only in 4 Parties which will be detailed below");
 
         Console.WriteLine("Party 1: APC(All Progressive Congress)");
@@ -26,7 +28,7 @@ class VotingFor{
         // FInally the Logic to determine Eligible Voting
         // using the C# comparison operator(==)
 
-        Console.WriteLine("Having Said all of That, If you would love to continue with Me Application, Press 'Y' for Yeah and 'N' No to exit the Applicatio");
+        Console.WriteLine("Having Said all of That, If you would love to continue with Me Application, Enter 'Y' for Yes and 'N' No to exit the Application");
 
         string usersAns = Console.ReadLine();
 
@@ -37,13 +39,57 @@ class VotingFor{
         // GetType() method that will both the classname and the namespace that classname exists in
 
 
-        string userAnsString = usersAns.GetTypeCode()
+        // string userAnsString = usersAns.GetTypeCode()
 
         if (usersAns == "Y")
         {
             Console.WriteLine("Thanks for Continuing to use Dynasty's Small but scalable Voting App");
-            Console.WriteLine("Thanks for Continuing to use Dynasty's Small but scalable Voting App");
+            Console.WriteLine("As we both Know, the Constitution of the Federal Republic of Nigeria says to exercise your Right to vote and be voted for, you must be 18 years of age and above");
+
+            Console.WriteLine("We as an Organization will now be fperforming our Background Check to confirm you are 18 years of Age and Above");
+
+            Console.WriteLine("To conntinue with this, Enter 'Y' for Yeah and 'N' No to exit the Application");
+
+            string appContinue = Console.ReadLine();
+
+            if (appContinue == "Y")
+            {
+                Console.WriteLine("Please answer the FOllowing Qustions");
+                Console.WriteLine("How Old Are you?");
+
+                string usersAge = Console.ReadLine();
+                // don't forget, the .ReadLine() method works with only strings and thats why we stored it in a string variable
+                // the Console.ReadLine() variable value needs to be converted into an Integer because we both know: Age is a number(or an Integer and not a string)
+
+                int intUsersAge = int.Parse(usersAge);
+
+                if (intUsersAge >= 18)
+                {
+
+                }
+                else if (intUsersAge == 17)
+                {
+                    Console.WriteLine("You only have one more year to go, Come back next Yer when you are older and cast your Vote");
+                }
+                else if (intUsersAge < 17)
+                {
+                    Console.WriteLine("I am deeply Sorry my User, our Organization has its Policies on only allowing People Older than 18 to cast their Votes.... YOu can Learn more on Voting rights by running a Google Search");
+                }
+
+
+            }
+            // exiting the Application for my user anytime they choose to
+            else if (usersAns == "N")
+            {
+                Console.WriteLine("Goodbye Me Dear User :)");
+            }
+
         }
+        else if (usersAns == "N")
+        {
+            Console.WriteLine("Goodbye Me Dear User :)");
+        }
+
 
 
 
